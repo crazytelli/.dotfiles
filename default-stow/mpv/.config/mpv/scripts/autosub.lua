e8949f24fe3bbbe7133a4d628b1653fa19f7a437
crazytelli <victorcadarso@gmail.com>
2022-02-10 22:53:39 -0300

    fix python subliminal package path from default to the user

--- default-stow/mpv/.config/mpv/scripts/autosub.lua
+++ default-stow/mpv/.config/mpv/scripts/autosub.lua
@@ -3,7 +3,7 @@
 --=============================================================================
 --          This script uses Subliminal to download subtitles,
 --          so make sure to specify your system's Subliminal location below:
-local subliminal = '/home/david/.local/bin/subliminal'
+local subliminal = '/home/crazytelli/.local/bin/subliminal'
 --=============================================================================
 -->>    SUBTITLE LANGUAGE:
 --=============================================================================
@@ -15,12 +15,12 @@ local languages = {
 --          other languages will NOT be downloaded,
 --          so put your preferred language first:
             { 'English', 'en', 'eng' },
-            { 'Dutch', 'nl', 'dut' },
+--            { 'Dutch', 'nl', 'dut' },
 --          { 'Spanish', 'es', 'spa' },
 --          { 'French', 'fr', 'fre' },
 --          { 'German', 'de', 'ger' },
 --          { 'Italian', 'it', 'ita' },
---          { 'Portuguese', 'pt', 'por' },
+          { 'Portuguese', 'pt', 'por' },
 --          { 'Polish', 'pl', 'pol' },
 --          { 'Russian', 'ru', 'rus' },
 --          { 'Chinese', 'zh', 'chi' },
