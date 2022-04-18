eefe849bda36ba7087a3e9d14e069b581d936139
crazytelli <victorcadarso@gmail.com>
2022-04-18 19:27:12 -0300

    fixed miniconda installer

--- default-stow/scripts/.scripts/installminiconda.sh
+++ default-stow/scripts/.scripts/installminiconda.sh
@@ -2,9 +2,10 @@
 
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
 
-sh ~/miniconda.sh
+sh ~/miniconda.sh -b -f -p  $HOME/.miniconda
 rm ~/miniconda.sh
 
-#source $HOME/miniconda3/etc/profile.d/conda.sh
+source $HOME/.miniconda/etc/profile.d/conda.sh
+$HOME/.miniconda/bin/conda init bash
 
 conda config --set auto_activate_base false
