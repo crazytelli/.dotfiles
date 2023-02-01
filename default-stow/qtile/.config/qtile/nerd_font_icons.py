e3a2839dc4ee249b8a64d982aecb94039abc1a23
victor loca <crazytelli@proton.me>
2023-02-01 18:24:28 -0300

    added nerd font icons file

---
+++ default-stow/qtile/.config/qtile/nerd_font_icons.py
@@ -0,0 +1,6 @@
+# Coleção de icones nerd font para acesso rápido.
+# Disponível em: https://www.nerdfonts.com/cheat-sheet
+
+battery_charging = ['󱟦', '󱟧', '󰂏', '󰂐']
+battery_discharging = ['󱟤', '󱟥', '󰂌', '󰂍']
+battery_full = ['󱟢', '󱟣']
