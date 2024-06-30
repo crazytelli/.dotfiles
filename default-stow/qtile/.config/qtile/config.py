294d4d7b853031d51aec0ca41047f937c3fb52f4
crazytelli <crazytelli@proton.me>
2024-06-30 20:22:24 -0300

    adição do layout monadtall e configurações gerais...

--- default-stow/qtile/.config/qtile/config.py
+++ default-stow/qtile/.config/qtile/config.py
@@ -2,7 +2,6 @@ from typing import List  # noqa: F401
 
 from libqtile import bar, layout, widget, qtile
 
-# from libqtile import layout
 from libqtile.config import Click, Drag, Group, Key, KeyChord, Match, Screen
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
@@ -33,7 +32,8 @@ colors = dracula.init_colors()
 # from myscreens import screens
 
 mod = "mod4"
-terminal = guess_terminal()
+# terminal = guess_terminal()
+terminal = "kitty"
 
 keys = [
     # Switch between windows
@@ -42,33 +42,32 @@ keys = [
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
     Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
-    Key(
-        [mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"
-    ),
-    Key(
-        [mod, "shift"],
-        "l",
-        lazy.layout.shuffle_right(),
-        desc="Move window to the right",
-    ),
+    Key( [mod, "shift"], "h", lazy.layout.shuffle_left().when(layout=['columns']), desc="Move window to the left"),
+    Key([mod, "shift"], "l", lazy.layout.shuffle_right().when(layout=['columns']), desc="Move window to the right",),
     Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
     Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key(
-        [mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"
-    ),
+    Key( [mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
     Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
     Key([mod], "b", lazy.hide_show_bar(), desc="Toggle hide/show the bar"),
-    Key(
-        [mod, "shift"],
-        "f",
-        lazy.window.toggle_floating(),
-        desc="Toggle floating",
-    ),
+    Key( [mod, "shift"], "f", lazy.window.toggle_floating(), desc="Toggle floating",),
+
+    # MonadTall default bindings
+    # Key([mod, "shift"], "h", lazy.layout.swap_left().when(layout=['monadtall'])),
+    # Key([mod, "shift"], "l", lazy.layout.swap_right().when(layout=['monadtall'])),
+    Key([mod, "shift"], "j", lazy.layout.shuffle_down().when(layout=['monadtall'])),
+    Key([mod, "shift"], "k", lazy.layout.shuffle_up().when(layout=['monadtall'])),
+    Key([mod], "i", lazy.layout.grow().when(layout=['monadtall'])),
+    Key([mod], "d", lazy.layout.shrink().when(layout=['monadtall'])),
+    Key([mod], "n", lazy.layout.reset().when(layout=['monadtall'])),
+    Key([mod], "o", lazy.layout.maximize().when(layout=['monadtall'])),
+    Key([mod, "shift"], "space", lazy.layout.flip().when(layout=['monadtall'])),
+
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
     # Unsplit = 1 window displayed, like Max layout, but still with
@@ -79,11 +78,12 @@ keys = [
     #     lazy.layout.toggle_split(),
     #     desc="Toggle between split and unsplit sides of stack",
     # ),
+
     Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
     # Toggle between different layouts as defined below
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
-    Key([mod], "m", lazy.window.toggle_fullscreen(), desc="Toggle fullscreen"),
-    Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
+    Key([mod], "f", lazy.window.toggle_fullscreen(), desc="Toggle fullscreen"),
+    Key([mod], "q", lazy.window.kill(), desc="Kill focused window"),
     Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
     Key([mod], "r", lazy.spawn("rofi -show drun"), desc="spawn rofi"),
@@ -91,7 +91,7 @@ keys = [
         [mod, "shift"],
         "r",
         lazy.spawncmd(),
-        desc="Spawn a command using a prompt widget",
+        desc="Spawn a command using a dmenu prompt widget",
     ),
     Key(
         [mod],
@@ -188,7 +188,7 @@ for i in groups:
 def init_layout_theme():
     return {
         "border_width": 3,
-        "margin": 4,
+        "margin": 20,
         "border_focus": colors[1],
         "border_normal": colors[13],
     }
@@ -199,8 +199,12 @@ layout_theme = init_layout_theme()
 
 # Layouts config
 layouts = [
-    layout.Columns(**layout_theme),
-    layout.Max(),
+    #layout.Columns(**layout_theme),
+    layout.MonadTall(**layout_theme, align=layout.MonadTall._left,
+),
+    layout.Max(
+        border_width = 0, 
+        margin = 0),
 ]
 
 widget_defaults = dict(
@@ -227,12 +231,13 @@ floating_layout = layout.Floating(
     ],
 )
 
-
 screens = [
     Screen(
         top=bar.Bar(
             [
                 widget.GroupBox(
+                    font="Hack Nerd Font Bold",
+                    fontsize=18,
                     highlight_method="line",
                     this_screen_border=colors[4],
                     this_current_screen_border=colors[11],
@@ -241,17 +246,21 @@ screens = [
                     background=colors[13],
                     hide_unused=True,
                 ),
-                widget.TextBox(text="", padding=0, fontsize=20, foreground="#2f343f"),
-                widget.Prompt(font="Hack Nerd Font Bold"),
-                widget.Spacer(length=10),
-                widget.WindowName(font="Hack Nerd Font Bold"),
+                # Dmenu run prompt
+                widget.CurrentLayoutIcon(scale=0.8),
+                widget.Prompt(
+                    font="Hack Nerd Font Bold",
+                    fontsize=18
+                    ),
+
+                widget.Spacer(length=20),
+                widget.WindowName(font="Hack Nerd Font Bold", fontsize=18),
                 widget.Chord(
                     chords_colors={
                         "launch": (colors[3], colors[10]),
                     },
                     name_transform=lambda name: name.upper(),
                 ),
-                widget.CurrentLayoutIcon(scale=0.65),
                 widget.CheckUpdates(
                     update_interval=1800,
                     distro="Arch_yay",
@@ -263,35 +272,30 @@ screens = [
                     background=colors[13],
                 ),
                 widget.Chord(),
-                widget.Systray(),
+                widget.Systray(icon_size=24, padding=5),
                 widget.Spacer(length=5),
-                widget.TextBox(text="", padding=0, fontsize=20, foreground="#2f343f"),
-                widget.TextBox(text="", padding=0, fontsize=20, foreground="#2f343f"),
                 widget.Memory(
                     font="Hack Nerd Font Bold",
-                    fontsize=14,
+                    fontsize=18,
                     # background=colors[13],
                     foreground=colors[6],
-                    format="󰍛{MemUsed: .0f} MB ",
+                    format="󰍛:{MemUsed:.0f} MB",
                     padding_y=4,
                 ),
-                widget.TextBox(text="", padding=0, fontsize=20, foreground="#2f343f"),
-                widget.TextBox(text="", padding=0, fontsize=20, foreground="#2f343f"),
-                # widget.TextBox(text="", padding=0, fontsize=20, foreground="#2f343f"),
                 widget.Clock(
                     format="%d-%m-%Y %a %H:%M",
                     foreground=colors[15],
                     font="Hack Nerd Font Bold",
-                    fontsize=14,
+                    fontsize=18,
                 ),
-                widget.TextBox(text="", padding=0, fontsize=20, foreground="#2f343f"),
-                widget.TextBox(text="", padding=0, fontsize=20, foreground="#2f343f"),
+                widget.Spacer(length=5),
                 widget.Battery(
                     battery=1,
                     charge_char="󱟦",
                     discharge_char="󱟤",
                     empty_char="",
-                    full_char="",
+                    #full_char="",
+                    full_char="󱟢",
                     unknown_char="",
                     font="Hack Nerd Font Bold",
                     fontsize=18,
